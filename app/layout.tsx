d533b6ebe6011177f5eafd04ec081e029e00752d
Sanan Buluktaev <s.veat.b@protonmail.com>
2026-03-11 21:50:17 +0300

    feat: add PWA manifest, icons and meta tags for installability

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,16 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "PushUp Tracker",
-  description: "Track your pushups with friends",
+  description: "Командный трекер отжиманий с автосчётом через камеру",
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "PushUp Tracker",
+  },
+  other: {
+    "mobile-web-app-capable": "yes",
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +28,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,1,-25&display=block" rel="stylesheet" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body>
         {/* Prevent FOUC: set dark class before first paint */}
