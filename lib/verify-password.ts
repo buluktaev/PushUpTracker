79e42f2889232af43d2b57372db3a588a46aefb4
Sanan Buluktaev <s.veat.b@protonmail.com>
2026-03-16 14:57:26 +0300

    fix: keep current session after room deletion

--- lib/verify-password.ts
+++ lib/verify-password.ts
@@ -13,7 +13,7 @@ export async function verifyPassword(email: string, password: string) {
   )
 
   const { error } = await supabase.auth.signInWithPassword({ email, password })
-  await supabase.auth.signOut()
+  await supabase.auth.signOut({ scope: 'local' })
 
   return !error
 }
