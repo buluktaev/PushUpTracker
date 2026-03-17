570594de5f439277853fdb3a3d794c70f37f2b9d
Sanan Buluktaev <s.veat.b@protonmail.com>
2026-03-17 15:23:06 +0300

    fix: use rejectUnauthorized instead of CA cert file for Yandex Cloud SSL

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import fs from 'fs'
-import path from 'path'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 function createPrismaClient() {
-  const ca = fs.readFileSync(path.join(process.cwd(), 'certs/yandex-ca.pem')).toString()
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
-    ssl: { ca },
+    ssl: { rejectUnauthorized: false },
   })
   return new PrismaClient({ adapter })
 }
